89efb791557651e9ca765dd3835d5298d3ceac93
Alma <arellano_alma@outlook.com>
2023-03-09 15:52:03 -0600

    Delete alma directory

--- alma/PostworkS2/src/main/java/Helado.java
+++
@@ -1,4 +0,0 @@
-public interface Helado {
-    public String getItem();
-    public int getPrice();
-}
--- alma/PostworkS2/src/main/java/HeladoSuave.java
+++
@@ -1,9 +0,0 @@
-public class HeladoSuave implements Helado {
-    @Override
-    public String getItem() {
-        return "Helado Suave";
-    }
-    public int getPrice() {
-        return 30;
-    }
-}
