a024a911d5116e3ff21db7920d09fe3ce934dac2
Marco Cardenas <bcv10g@hotmail.com>
2023-03-15 20:31:02 -0600

    Post funcional

--- postwork6/src/main/java/org/bedu/java/backend/Postwork/controller/PersonaController.java
+++ postwork6/src/main/java/org/bedu/java/backend/Postwork/controller/PersonaController.java
@@ -2,6 +2,7 @@ package org.bedu.java.backend.Postwork.controller;
 
 
 import org.bedu.java.backend.Postwork.model.Persona;
+import org.bedu.java.backend.Postwork.service.FormateadorTelefono;
 import org.bedu.java.backend.Postwork.service.ValidadorTelefono;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -19,10 +20,14 @@ public class PersonaController {
     @PostMapping
     public ResponseEntity<Persona> creaPersona(@RequestBody Persona persona){
         ValidadorTelefono validadorTelefono = new ValidadorTelefono();
+        persona.setTelefono(validadorTelefono.limpiaNumero(persona.getTelefono()));
         if(validadorTelefono.isValido(persona.getTelefono())) {
             System.out.println("Success!");
+            FormateadorTelefono formateadorTelefono = new FormateadorTelefono();
+            persona.setTelefono(formateadorTelefono.formatea(persona.getTelefono()));
             return ResponseEntity.status(HttpStatus.CREATED).body(persona);
         }
+
         System.out.println("not good");
         return ResponseEntity.status(HttpStatus.NOT_ACCEPTABLE).body(null);
     }
